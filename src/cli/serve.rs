a4740746b026bd7896ad4eeaca633dc41063d2b8
stewartli <stewardli8@msn.com>
2025-08-21 15:45:57 +0800

    add axum fallback

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,9 +1,16 @@
 use std::path::PathBuf;
 use serde::Deserialize;
 
-use axum::{extract::Query, http::StatusCode, response::{Html, IntoResponse, Response}, routing::get, Router};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
+
+use axum::{
+    extract::Query, 
+    http::StatusCode, 
+    response::{Html, IntoResponse, Response}, 
+    routing::get, 
+    Router, 
+};
 
 #[derive(Deserialize)]
 struct QmdReport {
@@ -15,16 +22,28 @@ pub fn run(addr: &str){
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
         let listener = TcpListener::bind(addr).await.unwrap();
-        println!("server listening on {}", listener.local_addr().unwrap());
+        println!("✓ server listening on {}", listener.local_addr().unwrap());
 
         let app = Router::new()
-            .route("/", get(|| async { "Hello, World!" }))
+            .route("/", get(|| async { "Financial Audit Tool" }))
+            // http://127.0.0.1:8090/report?client=beijing&year=2023
             .route("/report", get(show_report))
-            // /asset/index.html
-            .nest_service("/asset", ServeDir::new("asset"));
+            // http://127.0.0.1:8090/temp/src/main.rs
+            .nest_service("/temp", ServeDir::new("."))
+            .fallback(handle_404);
 
-        axum::serve(listener, app).await.unwrap();
+        axum::serve(listener, app)
+            .with_graceful_shutdown(stop_server())
+            .await
+            .unwrap();
+
+        println!("✓ server sucessfully shutdown");
     })
+}
+
+async fn stop_server(){
+    tokio::signal::ctrl_c().await.expect("fail Ctrl+C handler");
+    println!("\n✓ server starting shutdown ...");
 }
 
 async fn show_report(Query(qmd): Query<QmdReport>) -> Response {
@@ -34,12 +53,16 @@ async fn show_report(Query(qmd): Query<QmdReport>) -> Response {
         .join(qmd.client)
         .join(qmd.year)
         .join("report/report.html");
-    // http://127.0.0.1:8090/report?client=beijing&year=2023
+    // consider stream
     if path_report.exists(){
-        let res = std::fs::read_to_string(path_report).unwrap();
+        let res = std::fs::read_to_string(path_report).expect("fail to read path report");
         (StatusCode::OK, Html(res)).into_response()
     }else{
         (StatusCode::BAD_REQUEST, Html("nothing")).into_response()
     }
+}
+
+async fn handle_404() -> impl IntoResponse {
+    "no response for you"
 }
 
